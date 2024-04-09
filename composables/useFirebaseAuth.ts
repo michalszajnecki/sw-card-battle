8c0c560afb801fdbc0c13e5ea6b02cd298c4e62e
Michal Szajnecki <michal.szajnecki@gmail.com>
2024-04-09 18:42:19 +0100

    Firebase composables for auth and db

---
+++ composables/useFirebaseAuth.ts
@@ -0,0 +1,57 @@
+import { createUserWithEmailAndPassword, User, signInWithEmailAndPassword } from 'firebase/auth';
+
+export default function () {
+    const { $auth } = useNuxtApp();
+
+    const user = useCookie<User | null>('fb_user', {
+        default: () => null,
+        watch: true,
+    });
+
+    const registerUser = async (email: string, password: string): Promise<unknown> => {
+        try {
+            await logoutUser();
+            const userCreds = await createUserWithEmailAndPassword($auth, email, password);
+            if (userCreds) {
+                user.value = { uid: userCreds.user.uid, stsTokenManager: userCreds.user?.stsTokenManager };
+                return user.value;
+            }
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error(error);
+            }
+            return false;
+        }
+        return false;
+    };
+
+    const logoutUser = async (): Promise<boolean> => {
+        user.value = null;
+        return true;
+    };
+
+    const loginUser = async (email: string, password: string): Promise<unknown> => {
+        try {
+            await logoutUser();
+            const userCreds = await signInWithEmailAndPassword($auth, email, password);
+
+            if (userCreds) {
+                user.value = { uid: userCreds.user.uid, stsTokenManager: userCreds.user?.stsTokenManager };
+                return user.value;
+            }
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error(error);
+            }
+            return false;
+        }
+        return false;
+    };
+
+    return {
+        user,
+        registerUser,
+        loginUser,
+        logoutUser,
+    };
+}
